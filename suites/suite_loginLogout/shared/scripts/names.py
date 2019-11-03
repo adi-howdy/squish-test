2c8345ab1b3bcf0ce66a3363cc6b083a5583a8f8
qa <qa@CentOS50G.local>
2019-11-02 17:02:36 -0700

    test_login

---
+++ suites/suite_loginLogout/shared/scripts/names.py
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+
+from objectmaphelper import *
+
+jSonar_Login_Simplifying_Security_Login_BrowserTab = {"title": "jSonar - Login - Simplifying Security - Login", "type": "BrowserTab"}
+jSonar_Login_Simplifying_Security_Login_username_text = {"container": jSonar_Login_Simplifying_Security_Login_BrowserTab, "form": "j_id_l", "id": "username", "name": "username", "tagName": "INPUT", "type": "text", "visible": True}
+jSonar_Login_Simplifying_Security_Login_password_password = {"container": jSonar_Login_Simplifying_Security_Login_BrowserTab, "form": "j_id_l", "id": "password", "name": "password", "tagName": "INPUT", "type": "password", "visible": True}
+jSonar_Login_Simplifying_Security_Login_Sign_In_submit = {"container": jSonar_Login_Simplifying_Security_Login_BrowserTab, "form": "j_id_l", "id": "btn-login", "name": "btn-login", "simplifiedInnerText": "Sign In", "tagName": "BUTTON", "type": "submit", "visible": True}
+jSonar_SonarG_Simplifying_Security_BrowserTab = {"title": "jSonar - SonarG - Simplifying Security", "type": "BrowserTab"}
+jSonar_SonarG_Simplifying_Security_jSonar_Simplifying_Security_DIV = {"container": jSonar_SonarG_Simplifying_Security_BrowserTab, "simplifiedInnerText": "jSonar Simplifying Security", "tagName": "DIV", "visible": True}
---
+++ suites/suite_loginLogout/tst_loginlogout/test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import names
+import sys
+
+def for_each_call(frame, event, arg):
+    snooze(0.1)
+ 
+def init():
+    sys.setprofile(for_each_call)
+
+def main():
+    startBrowser("https://192.168.1.22:8443")
+    activateBrowserTab(waitForObject(names.jSonar_Login_Simplifying_Security_Login_BrowserTab))
+    setFocus(waitForObject(names.jSonar_Login_Simplifying_Security_Login_username_text))
+    typeText(waitForObject(names.jSonar_Login_Simplifying_Security_Login_username_text), "admin")
+    setFocus(waitForObject(names.jSonar_Login_Simplifying_Security_Login_password_password))
+    typeText(waitForObject(names.jSonar_Login_Simplifying_Security_Login_password_password), "jS0nar$")
+    clickButton(waitForObject(names.jSonar_Login_Simplifying_Security_Login_Sign_In_submit))
+    test.compare(waitForObjectExists(names.jSonar_SonarG_Simplifying_Security_jSonar_Simplifying_Security_DIV).simplifiedInnerText, "jSonar Simplifying Security")
